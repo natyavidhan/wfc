506fc7e155765f33d2802099c500a0b2144ecfee
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-07-05 20:02:05 +0530

    black magic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from PIL import Image
+import random
 
 class Tile:
     def __init__(self, img, side=32, rotate=0):
@@ -47,10 +48,18 @@ class WFC:
                     self.running = False
             self.screen.fill((0, 0, 0))
 
+            flat_grid = [x for y in self.grid for x in y if x.current is None]
+            if len(flat_grid) >= 1:
+                chosen_one = random.choice(flat_grid)
+                o = random.choice(chosen_one.options)
+                chosen_one.current = o
+
             for row in self.grid:
                 for cell in row:
                     if cell.current is None:
                         pygame.draw.rect(self.screen, (255, 255, 255), (cell.x*self.side, cell.y*self.side, self.side, self.side), 2)
+                    else:
+                        self.screen.blit(cell.current.img, (cell.x*self.side, cell.y*self.side))
 
             self.clock.tick(60)
             pygame.display.update()
